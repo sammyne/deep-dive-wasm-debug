b13c82c5128527ca53d38cf9f937524f49447fb3
xiangminli <xiangminli@tencent.com>
2025-04-27 21:45:04 +0800

    feat: add component demo

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -10,5 +10,6 @@ build_arg_opts="$build_arg_opts --build-arg WASI_SDK_MAJOR_VERSION=22"
 build_arg_opts="$build_arg_opts --build-arg WASI_SDK_MINOR_VERSION=0"
 build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=32.0.0"
 build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.229.0"
+build_arg_opts="$build_arg_opts --build-arg WIT_BINDGEN_VERSION=0.27.0"
 
 docker build $build_arg_opts -t $repo_tag .
